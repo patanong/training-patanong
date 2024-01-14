1a7562451c9adca55489aa6fbe28116fe9936129
unknown <patanong13@gmail.com>
2024-01-14 19:24:47 +0700

    first commit

---
+++ src/pages/Contact.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+const Contact = () => {
+  return (
+    <div>
+    <h1>ติดต่อ</h1>
+      <hr />
+      <h2>NVC Training Conter</h2>
+      <h5><b>Adress:</b>90 Tesa Rd.,Mueang,Nakhon Pathom 73000</h5>
+      <h5><b>Tel:</b>034-252790</h5>
+      <h5><b>E-mail:</b>admin@trainingconter.com</h5>
+    </div>
+  )
+}
+
+export default Contact
---
+++ src/pages/Home.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+const Contact = () => {
+  return (
+    <div>
+    <h1>หน้าหลัก</h1>
+      <hr />
+      <img src="001156.jpg" alt="" />
+    </div>
+  )
+}
+
+export default Contact
